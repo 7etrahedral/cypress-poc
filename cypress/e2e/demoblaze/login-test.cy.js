92a334e6929430471659ec0da34fb2fa453944d3
Genta Wibowo <genta.wibowo@bukalapak.com>
2023-02-22 08:52:19 +0700

    initiate version 12

---
+++ cypress/e2e/demoblaze/login-test.cy.js
@@ -0,0 +1,14 @@
+describe('Demoblaze app Login Scenario', () => {
+  it('Successfully login using correct username password', () => {
+    cy.visit('https://www.demoblaze.com/')
+
+    cy.get(`[data-target='#logInModal']`).click()
+    cy.get(`[onclick='logIn()']`).should('be.visible')
+    cy.wait(1000)
+    cy.get('#loginusername')
+      .type('wibowo.bullseye')
+      .should('have.value', 'wibowo.bullseye')
+    cy.get('#loginpassword').type('bullseye')
+    cy.get(`[onclick='logIn()']`).click()
+  })
+})
